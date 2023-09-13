3bea1d601e63662931a74718f47166be20c094cb
mohamedibrahim <mohamed.mohamed.yossef@gmail.com>
2023-09-13 22:53:31 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const showIcon = document.querySelector(".show");
+const closeIcon = document.querySelector(".close");
+const mobileMenu = document.querySelector("#mobile-view");
+
+mobileMenu.style.display = "none";
+closeIcon.style.display = "none";
+
+showIcon.addEventListener("click", e => {
+  e.preventDefault();
+
+  mobileMenu.style.display = "block";
+  showIcon.style.display = "none";
+  closeIcon.style.display = "block";
+});
+
+closeIcon.addEventListener("click", e => {
+  e.preventDefault();
+  mobileMenu.style.display = "none";
+  closeIcon.style.display = "none";
+  showIcon.style.display = "block";
+});
